aba0786e2fc3f0dede80760d88b7defa09e169e9
BEEFBOSS6969 <57959111+BEEFBOSS6969@users.noreply.github.com>
2019-11-26 10:10:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 input="add:"
- num1=float(input("Enter a number:"))
- num2=float(input("Enter another number:"))
- result=str(num1+num2)
- print(" the answer is"+ result)
+num1=float(input("Enter a number:"))
+num2=float(input("Enter another number:"))
+result=str(num1+num2)
+print(" the answer is"+ result)
