fba71a6f93a0d5b32b9701e663250da210d9e588
BEEFBOSS6969 <57959111+BEEFBOSS6969@users.noreply.github.com>
2019-12-06 12:04:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-num1 = float(input("Enter a number:"))
-num2 = float(input("Enter another number:"))
-result = str(num1+num2)
-print (" the answer is"+ result)
 user_input = input("operation:")
+if user_input == " add "
+ num1 = float(input("Enter a number:"))
+ num2 = float(input("Enter another number:"))
+ result = str(num1+num2)
+ print (" the answer is"+ result)
 if user_input == "subtract":
  num1 = float (input("Enter a number:"))
  num2 = float (input("Enter another number:"))
