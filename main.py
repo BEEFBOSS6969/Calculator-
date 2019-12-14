d8ed357cad372d46e51744763f79dfbb12a40543
BEEFBOSS6969 <57959111+BEEFBOSS6969@users.noreply.github.com>
2019-12-14 14:35:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -122,3 +122,10 @@ if user_input == "perimeter of a heptagon":
  side = float(input("Enter the length of one of the sides of the heptagon"))
  result = str(side*6)
  print (" The perimeter of the Heptagon is:" + result)
+if user_input == "perimeter of a octagon":
+ side = float(input("Enter the length of one of teh sides of the Octagon:"))
+ result = str(side*8)
+ print ("The perimeter of the octagon is:" + result)
+if user_input == "area of a octagon";
+
+                   
