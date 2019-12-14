80f75f1b120a207f4051b9c1d96471e6922a6b41
BEEFBOSS6969 <57959111+BEEFBOSS6969@users.noreply.github.com>
2019-12-14 14:24:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -116,9 +116,9 @@ if user_input == "perimeter of a hexagon":
 if user_input == "area of a heptagon":
  side = float(input("Enter the length of one of the sides of the Heptagon:"))
  perimeter = float(input(" Enter the perimeter of the heptagon:"))
- apothem = float(input("Enter a the distance fom the middle of the heptagon to the middleof a any side"))
+ apothem = float(input("Enter a the distance fom the middle of the heptagon to the middle of a any side"))
  result = str((perimeter*apothem)/2)
- print (" The area of the heptagon is" = result)
+ print (" The area of the heptagon is" + result)
 if user_input == "perimeter of a heptagon":
  side = float(input("Enter the length of one of the sides of the heptagon"))
  result = str(side*6)
