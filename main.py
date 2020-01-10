a03d8bdf5ae3c9f4a634a9170541fabb8aef7c6e
BEEFBOSS6969 <57959111+BEEFBOSS6969@users.noreply.github.com>
2020-01-10 10:02:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ if user_input == "Perimeter of a Kite":
     result = str((side_A*2)+(side_B*2))
               
 if user_input == "area of a pentagon":
-    side = float(input(" Enter the length of one of the sides of the pentagon:")
+    side = int(input(" Enter the length of one of the sides of the pentagon:")
     radius = int(input("Enter the distance from the center point to the middle point of any of the sides of the Pentagon:"))
     result = str((radius*5*side)/2)
               
