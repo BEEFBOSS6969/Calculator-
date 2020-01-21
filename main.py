9560865df34c209c2be3e4080c985944c47d2479
Karl.Kain <57959111+KarlKain@users.noreply.github.com>
2020-01-21 18:47:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -300,7 +300,7 @@ if user_input == "Trigonometric formula for tan(2a)":
     a = float(input("Enter the value of a:"))
     result = str((2*math.tan*a)/1-(math.tan*math.tan)*a)
                  
-if user_input== "trigonometric formula for cot(2a)":
+if user_input == "trigonometric formula for cot(2a)":
     a = float(input("Enter the value of a:"))
     result = str((math.cot*math.cot)(a-1)/(2*math.cot*a)
                  
